69e6d2bd962d32550ac41c761b2d004def1f004e
StephanSo <s.sonnois@gmail.com>
2017-10-10 16:42:36 +0200

    Ajout des differentes Views statiques

--- portailWeb/routes/index.js
+++ portailWeb/routes/index.js
@@ -9,5 +9,32 @@ router.get('/',
         res.render('index', { title: 'Portail de la M2L', user: req.user });
     }
 );
+router.get('/annuaire',
+    function (req, res) {
+        console.log(req.session);
+        res.render('annuaire', {title: 'Annuaire des ligues'});
+    }
+);
+
+router.get('/infos',
+    function (req, res) {
+        console.log(req.session);
+        res.render('infos', {title: 'Informations Utiles'});
+    }
+);
+
+router.get('/participationsJOP',
+    function (req, res) {
+        console.log(req.session);
+        res.render('participationsJOP', {title: 'Participation aux JO / JP'});
+    }
+);
+
+router.get('/espacesPresse',
+    function (req, res) {
+        console.log(req.session);
+        res.render('espacesPresse', {title: 'Espaces des presses'});
+    }
+);
 
 module.exports = router;
